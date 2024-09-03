bfb9e9e07cdc936966286797abc92877f105c1b9
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-03 19:39:36 +0900

    class, verify 컨트롤러 수정

--- EducationClassProject/src/main/java/com/example/EducationClassProject/controller/ClassController.java
+++ EducationClassProject/src/main/java/com/example/EducationClassProject/controller/ClassController.java
@@ -14,40 +14,41 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ClassController {
 
     private final ClassService classService;
 
     // class 생성
-    @PostMapping("/api/v1/class/{userId}") // 추후에 jwt 적용시 수정 필요
+    @PostMapping("/class/{userId}") // 추후에 jwt 적용시 수정 필요
     public BaseResponse<ClassResponseDTO.CreateClassResultDTO> createClass(@PathVariable UUID userId, @RequestBody ClassRequestDTO.CreateClassDTO createClassDTO) {
         Class classEntity = classService.createClass(createClassDTO, userId);
         return BaseResponse.onSuccess(ClassConverter.createClassResultDTO(classEntity));
     }
 
     // 유저 id로 class 찾기
-    @GetMapping("/api/v1/class/{userId}")
+    @GetMapping("/class/{userId}")
     public BaseResponse<ClassResponseDTO.PreviewClassListResultDTO> findClassesByUser(@PathVariable UUID userId) {
         List<Class> classes = classService.findClassesByUserId(userId);
         return BaseResponse.onSuccess(ClassConverter.toPreviewClassList(classes));
     }
 
     // class 전체 조회
-    @GetMapping("/api/v1/classes")
+    @GetMapping("/classes")
     public BaseResponse<ClassResponseDTO.PreviewClassListResultDTO> findAllClasses() {
         List<Class> classList = classService.findAllClasses();
         return BaseResponse.onSuccess(ClassConverter.toPreviewClassList(classList));
     }
 
     // 강의 삭제
-    @DeleteMapping("/api/v1/delete/class/{classId}")
+    @DeleteMapping("/delete/class/{classId}")
     public BaseResponse<String> deleteClass(@PathVariable Long classId) {
         classService.deleteClass(classId);
         return BaseResponse.onSuccess("삭제 되었습니다.");
     }
 
     // 강의 정보 수정
-    @PatchMapping("/api/v1/class/{classId}")
+    @PatchMapping("/class/{classId}")
     public BaseResponse<ClassResponseDTO.PreviewClassResultDTO> updateClass(@PathVariable Long classId, @RequestBody ClassRequestDTO.UpdateClassDTO updateClassDTO) {
         Class classEntity = classService.updateClass(classId, updateClassDTO);
         return BaseResponse.onSuccess(ClassConverter.previewClassResultDTO(classEntity));
--- EducationClassProject/src/main/java/com/example/EducationClassProject/controller/VerifyCardController.java
+++ EducationClassProject/src/main/java/com/example/EducationClassProject/controller/VerifyCardController.java
@@ -14,12 +14,13 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class VerifyCardController {
 
     private final VerifyService verifyService;
 
     // 인증서 신청 보내기
-    @PostMapping("/api/v1/verifyCard/{userId}") // jwt 추가 후 수정해야됨.
+    @PostMapping("/verifyCard/{userId}") // jwt 추가 후 수정해야됨.
     public BaseResponse<String> applyVerify(@PathVariable UUID userId, @RequestBody VerifyRequestDTO.ApplyVerifyDTO applyVerifyDTO) {
         VerifyCard verifyCard = verifyService.applyVerify(userId, applyVerifyDTO);
         return BaseResponse.onSuccess("선생 검증 요청을 보냈습니다");
@@ -27,14 +28,16 @@ public class VerifyCardController {
 
 
     // 인증서 내역 전체 조회 ( 관리자 페이지 )
-    @GetMapping("/api/v1/verifyCard")
+    @GetMapping("/verifyCard")
     public BaseResponse<VerifyResponseDTO.PreviewVerifyCardListDTO> previewVerifyCardList() {
         List<VerifyCard> verifyCardList = verifyService.previewVerifyCardList();
         return BaseResponse.onSuccess(VerifyCardConverter.previewVerifyCardList(verifyCardList));
     }
 
+    // 인증서 내역 조회 ( 미수락, 수락 )
+
     // 인증서 수락 ( 관리자 페이지 )
-    @PatchMapping("/api/v1/verify/accept/{verifyCardId}")
+    @PatchMapping("/verify/accept/{verifyCardId}")
     public BaseResponse<VerifyResponseDTO.PreviewVerifyCardDTO> acceptVerify(@PathVariable Long verifyCardId) {
         VerifyCard verifyCard = verifyService.acceptVerifyCard(verifyCardId);
         return BaseResponse.onSuccess(VerifyCardConverter.previewVerifyCardDTO(verifyCard));

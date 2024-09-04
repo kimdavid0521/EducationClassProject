5e86a7ffd1b03d53f5c2fcbeff5e83b0de76deee
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-04 17:49:07 +0900

    testFilter 추가

--- EducationClassProject/src/main/java/com/example/EducationClassProject/repository/ChatroomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EducationClassProject.repository;
-
-import com.example.EducationClassProject.domain.Chatroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
-}

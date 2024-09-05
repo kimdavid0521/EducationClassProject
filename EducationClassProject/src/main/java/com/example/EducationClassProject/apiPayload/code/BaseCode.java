3303e7ec5145521495f23f0bde530b68f6e6f7a1
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-05 09:24:44 +0900

    정보 securityContextHolder에 넣기

--- EducationClassProject/src/main/java/com/example/EducationClassProject/apiPayload/code/BaseCode.java
+++
@@ -1,8 +0,0 @@
-package com.example.EducationClassProject.apiPayload.code;
-
-public interface BaseCode {  // 답변에 사용할 뼈대 생성
-
-    public ReasonDTO getReason();
-
-    public ReasonDTO getReasonHttpStatus();
-}
--- EducationClassProject/src/main/java/com/example/EducationClassProject/domain/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.EducationClassProject.domain.enums;
-
-public enum Gender {
-    FEMALE, MALE, NONE
-}

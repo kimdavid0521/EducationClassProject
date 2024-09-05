a83f1088a47407c82b7a69ff0981ece5a1454971
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-05 09:21:14 +0900

    jwt token 검증

---
+++ EducationClassProject/src/main/java/com/example/EducationClassProject/global/filter/JWTFilter.java
@@ -0,0 +1,34 @@
+package com.example.EducationClassProject.global.filter;
+
+import com.example.EducationClassProject.global.PrincipalDetailService;
+import com.example.EducationClassProject.jwt.JWTUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+    private final PrincipalDetailService principalDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
+            String token = authorizationHeader.substring(7);
+
+            if (jwtUtil.isTokenValid(token)) {
+                String email = jwtUtil.getEmail(token);
+                UserDetails userDetails = principalDetailService.loadUserByUsername(email);
+            }
+        }
+    }
+}

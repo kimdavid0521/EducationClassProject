49f2f1323250b70fd83850ecd788a10d31e520e6
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-15 12:36:02 +0900

    로그인 api 구현

---
+++ EducationClassProject/src/main/java/com/example/EducationClassProject/domain/ChatMessage.java
@@ -0,0 +1,27 @@
+package com.example.EducationClassProject.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ChatMessage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatroom_id")
+    private Chatroom chatroom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User sender;
+}

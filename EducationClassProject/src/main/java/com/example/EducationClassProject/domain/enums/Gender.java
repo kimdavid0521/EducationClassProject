e6530804c4a17372e711ace3255a6144e1cd50c9
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-19 14:59:44 +0900

    class 생성, 참여시 유저 포인트 처리

--- EducationClassProject/src/main/java/com/example/EducationClassProject/domain/enums/Gender.java
+++
@@ -1,6 +0,0 @@
-package com.example.EducationClassProject.domain.enums;
-
-public enum Gender {
-    MALE, FEMALE, NONE
-
-}
--- EducationClassProject/src/main/java/com/example/EducationClassProject/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package com.example.EducationClassProject.service;
-
-import com.example.EducationClassProject.dto.orderDTO.OrderRequestDTO;
-
-public interface OrderService {
-
-    Long createOrder(OrderRequestDTO.CreateOrderDTO createOrderDTO, String token);
-}

287e236aa88792a4f9bb16ffdd5c5051c486bcf4
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-25 21:12:39 +0900

    test 수정

--- educationClassProject/src/main/java/com/example/EducationClassProject/controller/TestController.java
+++ educationClassProject/src/main/java/com/example/EducationClassProject/controller/TestController.java
@@ -18,7 +18,7 @@ public class TestController {
     }
 
 
-    //에러 터지는 코드 ( 수정 ) 1
+    //에러 터지는 코드 ( 수정 ) 12
     @GetMapping("/failed")
     public BaseResponse<String> failedTest() {
         testService.failedTest();

acdb187148165059233f07e175ee7c421b9a4d5e
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-26 14:48:36 +0900

    java17로 gradle 버전 변경

--- educationClassProject/src/main/java/com/example/EducationClassProject/controller/TestController.java
+++ educationClassProject/src/main/java/com/example/EducationClassProject/controller/TestController.java
@@ -18,7 +18,7 @@ public class TestController {
     }
 
 
-    //에러 터지는 코드 ( 수정 ) 123
+    //에러 터지는 코드 ( 수정 ) 1234
     @GetMapping("/failed")
     public BaseResponse<String> failedTest() {
         testService.failedTest();

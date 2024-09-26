481f8048bd5298280a7c2adb62140658a683fc8c
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-26 14:20:54 +0900

    build 폴더 밑에 libs 폴더 생성

--- educationClassProject/src/main/java/com/example/EducationClassProject/controller/TestController.java
+++ educationClassProject/src/main/java/com/example/EducationClassProject/controller/TestController.java
@@ -18,7 +18,7 @@ public class TestController {
     }
 
 
-    //에러 터지는 코드 ( 수정 ) 12
+    //에러 터지는 코드 ( 수정 ) 123
     @GetMapping("/failed")
     public BaseResponse<String> failedTest() {
         testService.failedTest();
